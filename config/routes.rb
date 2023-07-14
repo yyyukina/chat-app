b141b3b0f60aff800d2c35f66a77bf967b6edd4c
yyyukina <138771405+yyyukina@users.noreply.github.com>
2023-07-14 22:55:28 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
